ea27026c24ffd1fabd278ac39f95c4cde269381d
MoiseDeCarvalho <moise@moise.it>
2020-06-24 23:01:17 +0200

    aggiunto devise e link alle pagine in header

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,11 @@ class PagesController < ApplicationController
   def about
 
   end
+
+  def search
+
+  end
+
+  def nuovo_passaggio
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get 'about' => 'pages#about'
+
+  get 'search' => 'pages#search'
+
+  get 'nuovo_passaggio' => 'pages#nuovo_passaggio'
 end
