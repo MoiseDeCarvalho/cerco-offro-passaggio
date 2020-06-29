d9aae8dce0e745d3343034360f8bfada4606d441
MoiseDeCarvalho <moise@moise.it>
2020-06-29 23:52:45 +0200

    aggiunti campi username, name, surname, address, city, mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+	#protect_from_forgery with: exception
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:surname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:address])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:city])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:mobile])
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_23_063121) do
+ActiveRecord::Schema.define(version: 2020_06_29_214955) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2020_06_23_063121) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.string "name", default: ""
+    t.string "surname"
+    t.string "address"
+    t.string "city"
+    t.string "mobile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
