84990bf735030c0b63ccbb6c1b6b43b9688900e1
MoiseDeCarvalho <moise@moise.it>
2020-06-30 08:55:13 +0200

    rimosso avatar in tabella user

---
+++ db/migrate/20200630064726_remove_avatar_file_size_to_users.rb
@@ -0,0 +1,4 @@
+class RemoveAvatarFileSizeToUsers < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
---
+++ db/migrate/20200630064759_remove_avatar_updated_at_to_users.rb
@@ -0,0 +1,4 @@
+class RemoveAvatarUpdatedAtToUsers < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
